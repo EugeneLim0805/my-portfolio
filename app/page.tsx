34bb5ed1f900e1eb4cbf10efa7bfd8f0bd04b7f4
EugeneLim0805 <eugenelim0805@gmail.com>
2025-08-12 16:30:51 -0700

    Add Navbar and update main page layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import About from "./components/About";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -13,7 +15,10 @@ export default function Home() {
       <Hero />
       <About />
       <Experience />
+      <Projects />
       <Skills />
+      <Contact />
+      <Footer />
     </div>
   );
 }
